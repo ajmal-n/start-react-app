910134ecf6ae95f58064665cf4feff21c566ce80
anoopmtrois <anoopm@trois.com>
2022-04-04 12:09:52 +0530

    useREf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FRParent from './components/FRParent'
+import Ref from './components/Ref'
 
 function App() {
   return (
     <div styles={{display: "flex", justifyContent: "center"}} >
         <p> App created by ajmal</p>
-        <FRParent />
+        <Ref />
     </div>
 
   )
---
+++ src/components/Ref.jsx
@@ -0,0 +1,25 @@
+import React, {useRef} from 'react'
+
+function Ref() {
+  const usernameRef = useRef(null)
+  const passwordRef = useRef(null)
+  const submitRef = useRef(null)
+  const usernameKeyDown = (e)=> {
+    if(e.key === "Enter") passwordRef.current.focus()
+  }
+  const passwordKeyDown = (e)=> {
+    if(e.key === "Enter") submitRef.current.focus()
+  }
+  const submitKeyDown = (e)=> {
+    if(e.key === "Enter") alert("Form submitted")
+  }
+  return (
+    <div>
+        <input type="text" ref={usernameRef} onKeyDown={usernameKeyDown} placeholder='Enter username' />
+        <input type="text" ref={passwordRef} onKeyDown={passwordKeyDown} placeholder='Enter password' />
+        <button ref={submitRef} onKeyDown={submitKeyDown}  >Submit</button>
+    </div>
+  )
+}
+
+export default Ref
